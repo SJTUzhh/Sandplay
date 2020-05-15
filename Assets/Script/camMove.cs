e4a9c9b9235b8237a211c5345c585f1768760871
ChenRaine <406943353@qq.com>
2020-05-16 02:45:29 +0800

    视角切换时增加起降动画
    增加起飞降落动画及脚本

--- Assets/Script/camMove.cs
+++ Assets/Script/camMove.cs
@@ -15,4 +15,5 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+    
 }
--- Assets/Script/mainCam.cs
+++ Assets/Script/mainCam.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Playables;
 public class mainCam : MonoBehaviour
 {
     public GameObject mainCamera;
     public GameObject moveCamera;
-    // Start is called before the first frame update
+    public PlayableDirector playableDirector;
+    public PlayableDirector back_playableDirector;
+// Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera.SetActive(true);
+        moveCamera.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,15 +21,31 @@ public class mainCam : MonoBehaviour
         
     }
 
-    public void onClick_main()
+    void play()
+    {
+        playableDirector.Play();
+    }
+    void backplay()
+    {
+        back_playableDirector.Play();
+    }
+    void change()
     {
         mainCamera.SetActive(true);
         moveCamera.SetActive(false);
+    }
+    public void onClick_main()
+    {
+        backplay();       
+        Invoke("change",2f);
     }
     public void onClick_move()
     {
         mainCamera.SetActive(false);
         moveCamera.SetActive(true);
+        // animation.Play();
+        play();
     }
+
     
 }
