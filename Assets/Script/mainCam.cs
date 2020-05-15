dcc61d632eab3af908cd334d62e62b5fc51de2a9
ChenRaine <406943353@qq.com>
2020-05-16 00:43:06 +0800

    camera change
    修改了主相机和跟随相机

---
+++ Assets/Script/mainCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCam : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject moveCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick_main()
+    {
+        mainCamera.SetActive(true);
+        moveCamera.SetActive(false);
+    }
+    public void onClick_move()
+    {
+        mainCamera.SetActive(false);
+        moveCamera.SetActive(true);
+    }
+}
